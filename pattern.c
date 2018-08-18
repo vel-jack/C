94638619f1a3570a6f381ed5628498a290d8de94
Velmurugan R <cenavel2@gmail.com>
2018-08-18 16:14:51 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+  int n,k,l,x,y;
+  scanf("%d",&n);
+  for(int i=n,y=1;i>0;i--,y++){
+    x=0;
+    for(int j=0,l=y;j<i;j++,x--){
+      printf("%d\t",l);
+      if(x==0){
+        x=n;
+      }
+      l=l+x;
+    }
+    printf("\n");
+  }
+  return 0;
+}
